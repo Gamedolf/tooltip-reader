f95263f45df53e2706650f5da1536a71f5638a71
Gamedolf <gamedolf@live.co.uk>
2022-12-19 00:47:54 +0000

    Update app output

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,67 @@
-import * as a1lib from "@alt1/base"
-import TooltipReader from "./tooltipreader";
+import TooltipReader from "@alt1/tooltip"
+import { captureHold } from "@alt1/base"
+import { findReadLine, ColortTriplet } from "@alt1/ocr"
 
-require("!file-loader?name=[name].[ext]!./index.html");
-require("!file-loader?name=[name].[ext]!./appconfig.json");
+require("!file-loader?name=[name].[ext]!./index.html")
+require("!file-loader?name=[name].[ext]!./appconfig.json")
 
-const output = document.getElementById("output");
+const output = document.getElementById("output")
 
-const reader = new TooltipReader()
+const tr = new TooltipReader()
+tr.farTooltip = true
 
-export function capture() {
-    const img = a1lib.captureHoldFullRs()
-    if (!reader.pos) reader.find(img)
-    const text = reader.read(img)
-    output.innerHTML = `<div>${text}</div>`
+function createCanvas(imageData, width, height) {
+  const canvas = document.createElement("canvas")
+  canvas.width = width
+  canvas.height = height
+  const ctx = canvas.getContext("2d")
+  ctx.putImageData(imageData, 0, 0)
+  document.getElementById("image").innerHTML = ""
+  document.getElementById("image").appendChild(canvas)
 }
 
-const interval = setInterval(() => {
-    capture()
+const colors: ColortTriplet[] = [
+  [238, 218, 178], // test
+  [235, 224, 188], // ~white
+  [0, 255, 255], // interactive scenery
+  [248, 213, 107], // memb item
+  [184, 209, 209], // nonmemb item
+  [255, 255, 0] // npc
+]
+
+const font1 = require("@alt1/ocr/fonts/aa_8px_mono_allcaps.js")
+const font2 = require("@alt1/ocr/fonts/aa_8px_mono_new.js")
+const font3 = require("@alt1/ocr/fonts/aa_8px_mono_pof.js")
+const font4 = require("@alt1/ocr/fonts/aa_8px_mono_pof2.js")
+const font5 = require("@alt1/ocr/fonts/aa_8px_mono.js")
+const font6 = require("@alt1/ocr/fonts/aa_8px_new.js")
+const font7 = require("@alt1/ocr/fonts/pixel_8px_mono.js")
+const font8 = require("./assets/tooltipfont.fontmeta.json")
+
+tr.track((state) => {
+  const { x, y, width, height } = state.area
+
+  const image = captureHold(x, y + 18, width, height - 18)
+  const data = image.toData()
+
+  createCanvas(data, width, height)
+
+  const text1 = findReadLine(data, font1, colors, width, height)
+  const text2 = findReadLine(data, font2, colors, width, height)
+  const text3 = findReadLine(data, font3, colors, width, height)
+  const text4 = findReadLine(data, font4, colors, width, height)
+  const text5 = findReadLine(data, font5, colors, width, height)
+  const text6 = findReadLine(data, font6, colors, width, height)
+  const text7 = findReadLine(data, font7, colors, width, height)
+  const text8 = findReadLine(data, font8, colors, width, height)
+  console.log(text1)
+  console.log(text2)
+  console.log(text3)
+  console.log(text4)
+  console.log(text5)
+  console.log(text6)
+  console.log(text7)
+  console.log(text8)
 }, 1000)
 
 if (window.alt1) alt1.identifyAppUrl("./appconfig.json")
--- src/tooltipreader.ts
+++ src/tooltipreader.ts
@@ -1,55 +1,59 @@
-import * as a1lib from "@alt1/base"
+import { capture, captureHoldFullRs, RectLike } from "@alt1/base"
 import { ImgRef, ImageDetect } from "@alt1/base"
-import * as OCR from "@alt1/ocr"
+import { findReadLine } from "@alt1/ocr"
 
 const font = require("./assets/tooltipfont.fontmeta.json")
 
-const textcol:[number,number,number] = [238,218,178]
+const textcol: [number, number, number] = [238, 218, 178]
 
 const imgs = ImageDetect.webpackImages({
-    topLeft: require("./assets/top-left.png"),
-    topRight: require("./assets/top-right.png"),
-    bottomLeft: require("./assets/bottom-left.png"),
-    bottomRight: require("./assets/bottom-right.png"),
+  topLeft: require("./assets/top-left.data.png"),
+  topRight: require("./assets/top-right.data.png"),
+  bottomLeft: require("./assets/bottom-left.data.png"),
+  bottomRight: require("./assets/bottom-right.data.png")
 })
 
 export default class TooltipReader {
-    pos: a1lib.RectLike | null = null;
+  pos: RectLike | null = null
 
-    find(img?: ImgRef) {
-        console.log("find")
-        if (!img) { img = a1lib.captureHoldFullRs() }
-
-        const topLeft = img.findSubimage(imgs.topLeft)
-        const topRight = img.findSubimage(imgs.topRight)
-        const bottomLeft = img.findSubimage(imgs.bottomLeft)
-        const bottomRight = img.findSubimage(imgs.bottomRight)
-        if (!topLeft.length || !topRight.length || !bottomLeft.length || !bottomRight.length) return null
-        if (topLeft.length > 0 || topRight.length > 0 || bottomLeft.length > 0 || bottomRight.length > 0) {
-            console.log("Multiple tooltips found")
-        }
-
-        const width = topRight[0].x - topLeft[0].x - imgs.topLeft.width
-        const height = bottomRight[0].y - topRight[0].y - imgs.topRight.height
-
-        this.pos = {
-            x: topLeft[0].x + imgs.topLeft.width,
-            y: topLeft[0].y,
-            width,
-            height,
-        }
-
-        return this.pos
+  find(img?: ImgRef) {
+    console.log("find")
+    if (!img) img = captureHoldFullRs()
+    const topLeft = img.findSubimage(imgs.topLeft)
+    const topRight = img.findSubimage(imgs.topRight)
+    const bottomLeft = img.findSubimage(imgs.bottomLeft)
+    const bottomRight = img.findSubimage(imgs.bottomRight)
+    console.log("topLeft", topLeft)
+    console.log("topRight", topRight)
+    console.log("bottomLeft", bottomLeft)
+    console.log("bottomRight", bottomRight)
+    if (!topLeft.length || !topRight.length || !bottomLeft.length || !bottomRight.length) return null
+    if (topLeft.length > 1 || topRight.length > 1 || bottomLeft.length > 1 || bottomRight.length > 1) {
+      console.log("Multiple tooltips found")
     }
 
-    read(img?: ImgRef) {
-        console.log("read")
-        if (!this.pos) return null
-        let buf: ImageData
-        if (!img) buf = a1lib.capture(this.pos)
-        else buf = img.toData(this.pos.x, this.pos.y, this.pos.width, this.pos.height)
-        const text = OCR.findReadLine(buf, font, [textcol], Math.round(this.pos.width/2), Math.round(this.pos.height/2))
+    const width = topRight[0].x - topLeft[0].x - imgs.topLeft.width
+    const height = bottomRight[0].y - topRight[0].y - imgs.topRight.height
 
-        return text ?? null
+    this.pos = {
+      x: topLeft[0].x + imgs.topLeft.width,
+      y: topLeft[0].y,
+      width,
+      height
     }
+
+    return this.pos
+  }
+
+  read(img?: ImgRef) {
+    console.log("read")
+    if (!this.pos) return null
+    let buf: ImageData
+    if (!img) buf = capture(this.pos)
+    else buf = img.toData(this.pos.x, this.pos.y, this.pos.width, this.pos.height)
+    console.log("buf", buf)
+    const text = findReadLine(buf, font, [textcol], Math.round(this.pos.width / 2), Math.round(this.pos.height / 2))
+
+    return text ?? null
+  }
 }
